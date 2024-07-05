0d5f7b0d85c18c755f84669d50b922e40ca7caf4
Anna2791 <annushka279@mail.ru>
2024-07-05 17:45:59 +0300

    add all const

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-install pygame
+import pygame
+import random
+
 
 pygame.init()
 
@@ -6,8 +8,15 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGT = 600
 screen= pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGT))
 pygame.display.set_caption('Игра ТИР')
+icon = pygame.image.load('img/0000978_moorhuhn-crazy-chicken-digital-version-.jpg')
 
+target_img = pygame.image.load('img/target.png')
+target_width = 50
+target_height = 50
 
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+
+color = (random.randint(0,255),random.randint(0,255), random.randint(0,255))
 
 
 
