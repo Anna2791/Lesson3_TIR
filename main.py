de2c7d87b4d7547d927b310899ce76777e1800ad
Anna2791 <annushka279@mail.ru>
2024-07-05 17:07:46 +0300

    add const

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ install pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGT = 600
+screen= pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGT))
+pygame.display.set_caption('Игра ТИР')
+
+
+
+
 
 running = True
 while running:
