2490fbf097451cdbcd538f71548a2f043dcbeb4f
Anna2791 <annushka279@mail.ru>
2024-07-07 17:45:16 +0300

    finish cycle

--- main.py
+++ main.py
@@ -24,14 +24,14 @@ running = True
 while running:
     screen.fill(color)
     for event in pygame.event.get():
-        if event.tipe == pygame.QUIT:
+        if event.type == pygame.QUIT:
             running = False
-        if event.tipe == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGT - target_height)
-    screen.blit(cucum_png,(target_x, target_y))
+    screen.blit(target_img,(target_x, target_y))
     pygame.display.update()
     
 
