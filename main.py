d7f717f4dab1671824f4c6749fe473d7682def47
Anna2791 <annushka279@mail.ru>
2024-07-07 17:36:13 +0300

    create cycle

--- main.py
+++ main.py
@@ -15,12 +15,28 @@ target_width = 50
 target_height = 50
 
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
-
+target_y = random.randint(0, SCREEN_HEIGT - target_height)
 color = (random.randint(0,255),random.randint(0,255), random.randint(0,255))
 
 
 
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.tipe == pygame.QUIT:
+            running = False
+        if event.tipe == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGT - target_height)
+    screen.blit(cucum_png,(target_x, target_y))
+    pygame.display.update()
+    
+
+
+
+
+
 pygame.quit()
